bcd9957fc5b7f1dd47d4ad129ef0b462f55b2e8b
Service Web <sw@gssmail.com>
2024-03-08 16:41:21 -0600

    Commit changes in C:\Websites\Git\emarlow\3825\GCG_3825_DEF_LOGO.sql

---
+++ GCG_3825_DEF_LOGO.sql
@@ -0,0 +1,12 @@
+/* ==========================================
+ * TABLE: GCG_3825_DEF_LOGO
+ * ========================================== */
+CREATE TABLE "GCG_3825_DEF_LOGO"(
+ "ID" IDENTITY DEFAULT '0',
+ "LOGO_PATH" VARCHAR(255),
+ "column0" IDENTITY DEFAULT '0',
+ "column1" VARCHAR(255),
+ PRIMARY KEY ("ID"),
+ UNIQUE ("ID"));
+
+
